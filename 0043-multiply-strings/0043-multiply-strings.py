b1d90712666abf076bdb77ed28a998c19fbf5c40
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2024-09-07 21:30:35 +0530

    Time: 14 ms (75.97%), Space: 11.8 MB (6.21%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        s=num1+'*'+num2
+        return str(eval(s))
+        
