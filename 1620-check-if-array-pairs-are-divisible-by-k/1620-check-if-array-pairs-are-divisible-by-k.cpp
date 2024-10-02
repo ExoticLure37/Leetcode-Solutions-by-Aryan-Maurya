3090b2274c2d2a1a9b704336e6bd268a1954e8d4
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2024-10-02 10:26:18 +0530

    Time: 295 ms (5.13%), Space: 88.1 MB (5.39%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         map<int,int> mp;
         int n=arr.size();
         for(int i=0;i<n;i++)
