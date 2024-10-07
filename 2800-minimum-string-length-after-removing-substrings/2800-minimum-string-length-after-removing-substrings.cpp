30599c47184cdc7761a9a4324a85e023627bc395
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2024-10-07 09:30:23 +0530

    Time: 3 ms (89.38%), Space: 12.2 MB (5.98%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int minLength(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         stack<int> stk;
-    for(auto c: s){
-        if(c == 'B' && stk.size() && stk.top() == 'A') stk.pop();
-        else if(c == 'D' && stk.size() && stk.top() == 'C') stk.pop();
-        else stk.push(c);
-    }
-    return stk.size();
+        for(auto c: s){
+            if(c == 'B' && stk.size() && stk.top() == 'A') stk.pop();
+            else if(c == 'D' && stk.size() && stk.top() == 'C') stk.pop();
+            else stk.push(c);
+        }
+        return stk.size();
     }
 };
