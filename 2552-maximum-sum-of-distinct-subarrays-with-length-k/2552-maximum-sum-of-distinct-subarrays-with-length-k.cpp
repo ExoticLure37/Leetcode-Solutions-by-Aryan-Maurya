db0d8b040c0f17341fa006838eefc2aff6561996
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2024-11-16 14:27:19 +0530

    Time: 255 ms (9.23%), Space: 104.3 MB (8.99%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n=nums.size();
         int l=0,r=0;
-        deque<int> arr;
         map<int,int> mp;
         long long sum=0;
         while(r<k){
@@ -14,20 +16,14 @@ public:
         long long res=0;
         if(mp.size()==k) res=max(res,sum);
         else    res=max(res,0ll);
-            // cout<<"res="<<res<<endl;
         while(r<n){
-            // for(auto c:mp)  cout<<c.first<<" ";
-            // cout<<endl;
             mp[nums[l]]--;
             if(mp[nums[l]]==0)  mp.erase(nums[l]);
             sum-=nums[l];
             l++;
             mp[nums[r]]++;
             sum+=nums[r];
-            // for(auto c:mp)  cout<<c.first<<" ";
-            // cout<<endl;
             if(mp.size()==k)    res=max(res,sum);
-            // cout<<"res="<<res<<endl;
             r++;
         }
         return res;
