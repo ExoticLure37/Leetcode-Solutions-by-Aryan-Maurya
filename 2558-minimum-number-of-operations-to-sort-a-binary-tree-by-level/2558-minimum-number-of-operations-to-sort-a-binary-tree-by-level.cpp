36e5a4c8572ce546e46ea14120be6797d90ef7fb
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2024-12-23 11:02:13 +0530

    Time: 71 ms (79.76%), Space: 191.5 MB (49.56%) - LeetHub

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -30,6 +30,7 @@ public:
         return cnt;
     }
     int minimumOperations(TreeNode* root) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         int res=0;
         if(root==nullptr)   return res;
         queue<TreeNode*> q;
