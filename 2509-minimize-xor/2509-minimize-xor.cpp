e2c5cee0e48864958b5c995e0f96a511d26aec10
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2025-01-15 11:14:57 +0530

    Time: 2 ms (7.01%), Space: 8.1 MB (40.19%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int p=__builtin_popcount(num2);
+        cout<<"p="<<p<<endl;
+        int x=0;
+        for(int i=31;i>=0;i--){
+            if((num1>>i)&1){
+                if(p>0){
+                    x+=(1<<i);
+                    p--;
+                }
+            }
+        }
+        if(p==0)    return x;
+        for(int i=0;i<31;i++){
+            if(((num1>>i)&1)==0){
+                if(p>0){
+                    x+=(1<<i);
+                    p--;
+                }
+            }
+        }
+        return x;
+    }
+};
