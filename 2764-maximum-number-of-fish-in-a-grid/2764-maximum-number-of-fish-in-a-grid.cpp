c9786db72025c17b056c2adc988325fecdead983
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2025-01-28 11:53:33 +0530

    Time: 22 ms (26.09%), Space: 96.6 MB (56.04%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -3,41 +3,48 @@ public:
     int n,m;
     int dx[4]={-1,0,+1,0};
     int dy[4]={0,+1,0,-1};
-    void dfs(int x,int y,vector<vector<int> > & grid,vector<vector<int> > &vis,int& ans)
-    {
+
+    bool valid(int nx,int ny){
+        return nx>=0 && nx<n && ny>=0 && ny<m;
+    }
+
+    void dfs(int x,int y, vector<vector<int> > &vis,vector<vector<int> > &grid,int& sum){
         vis[x][y]=1;
-        ans+=grid[x][y];
-        for(int k=0;k<4;k++)
-        {
+
+        sum += grid[x][y];
+        
+        for(int k=0;k<4;k++){
             int nx = x+dx[k];
             int ny = y+dy[k];
 
-            if(nx>=0 && nx<n && ny>=0 && ny<m && !vis[nx][ny] && grid[nx][ny]!=0)
-            {
-                dfs(nx,ny,grid,vis,ans);
+            if(valid(nx,ny) && vis[nx][ny]==0 && grid[nx][ny]!=0){
+                dfs(nx,ny,vis,grid,sum);
             }
         }
+
+        return ;
     }
+
     int findMaxFish(vector<vector<int>>& grid) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        n=grid.size();
-        m=grid[0].size();
-        vector<vector<int> > vis(n,vector<int> (m,0));
+        n = grid.size();
+        m = grid[0].size();
+
         int res=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(!vis[i][j] && grid[i][j]!=0)
-                {
-                    int val = 0;
-                    dfs(i,j,grid,vis,val);
-                    res = max(res,val);
+
+        vector<vector<int> > vis(n,vector<int> (m,0));    
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]!=0){
+                    int sum = 0;
+
+                    dfs(i,j,vis,grid,sum);
+                    res=max(res,sum);
+                    cout<<endl;
                 }
             }
         }
+
         return res;
     }
 };
