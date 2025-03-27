8d932389f9635e1462c2bce32f85fcf4c944811b
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2025-03-27 21:36:49 +0530

    Time: 25 ms (46.89%), Space: 87.8 MB (66.28%) - LeetHub

--- 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.cpp
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<int>> dp;
-    
-    int solve(int idx, int i, vector<int> &nums, vector<int> &mul) {
-        if (idx == m) return 0;
-        if (dp[idx][i] != INT_MIN) return dp[idx][i];
+    vector<vector<int> > dp;
 
-        int j = n - 1 - (idx - i);
+    int solve(int idx, int i ,vector<int> &nums, vector<int> &mul) {
+        if (idx == m || i==n) {
+            return 0;
+        }
 
-        int left = nums[i] * mul[idx] + solve(idx + 1, i + 1, nums, mul);
-        int right = nums[j] * mul[idx] + solve(idx + 1, i, nums, mul);
+        if(dp[i][idx]!=-1)  return dp[i][idx];
 
-        return dp[idx][i] = max(left, right);
+        int j=n-1-idx+i;
+
+        int front = nums[i];
+        int back = nums[j];
+
+        int x = front * mul[idx];
+        int y = back * mul[idx];
+
+        int a = INT_MIN, b = INT_MIN;
+
+        a = solve(idx + 1, i + 1, nums, mul) + x;
+        b = solve(idx + 1, i, nums, mul) + y;
+
+        return dp[i][idx]=max(a, b);
     }
-    
+
     int maximumScore(vector<int>& nums, vector<int>& mul) {
         n = nums.size();
         m = mul.size();
 
-        dp.resize(m, vector<int>(m, INT_MIN));
+        dp.resize(m,vector<int> (m,-1));
 
         return solve(0, 0, nums, mul);
     }
