8dabb385131f5e08196ca3c7207bd458e482f804
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2025-04-01 19:45:10 +0530

    Time: 43 ms (8.89%), Space: 71.5 MB (5.18%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -1,77 +1,88 @@
 class Solution {
 public:
-    bool hasCycle(int src, vector<set<int>>& adj, vector<int>& vis) {
-        vis[src] = 1;
+    bool DFS(int src, vector<set<int>>& adj, vector<int>& vis) {
+        vis[src] = 1; 
+
         for (auto neigh : adj[src]) {
-            if (vis[neigh] == 1) return true;
-            if (vis[neigh] == 0 && hasCycle(neigh, adj, vis)) return true;
+            if (vis[neigh] == 1) return true; 
+            if (vis[neigh] == 0 && DFS(neigh, adj, vis)) return true; 
         }
-        vis[src] = 2;
+
+        vis[src] = 2;  
         return false;
     }
-
-    vector<int> topologicalsort(vector<set<int>>& adj, int k) {
+    vector<int> topologicalsort(vector<set<int> >& adj,int k){
         vector<int> order;
-        vector<int> inDegree(k+1, 0);
-        for (int i = 1; i <= k; i++) {
-            for (auto neigh : adj[i]) {
-                inDegree[neigh]++;
-            }
+        vector<int> indeg(k+1,0);
+        for(int i=1;i<=k;i++){
+            for(auto c:adj[i])  indeg[c]++;
         }
+
         queue<int> q;
-        for (int i = 1; i <= k; i++) {
-            if (inDegree[i] == 0) q.push(i);
+        for(int i=1;i<=k;i++){
+            if(indeg[i]==0)   q.push(i);
         }
-        while (!q.empty()) {
-            int node = q.front();
+
+        while(!q.empty()){
+            int node=q.front();
             q.pop();
             order.push_back(node);
-            for (auto neigh : adj[node]) {
-                inDegree[neigh]--;
-                if (inDegree[neigh] == 0) q.push(neigh);
+
+            for(auto neigh:adj[node]){
+                indeg[neigh]--;
+                if(indeg[neigh]==0)   q.push(neigh);
             }
         }
+
         return order;
-    }
-
+    }        
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        vector<set<int>> adjrow(k+1);
-        vector<set<int>> adjcol(k+1);
+        vector<set<int> > adjrow(k+1);    
 
-        for (auto& row : rowConditions) {
-            adjrow[row[0]].insert(row[1]);
+        for(auto row:rowConditions){
+            int u=row[0],v=row[1];
+            adjrow[u].insert(v);
         }
 
-        vector<int> vis(k+1, 0);
-        for (int i = 1; i <= k; i++) {
-            if (vis[i] == 0 && hasCycle(i, adjrow, vis)) {
+        vector<int> vis(k+1,0);
+        for(int i=1;i<=k;i++){
+            if(vis[i]==0 && DFS(i,adjrow,vis)){
                 return {};
             }
         }
 
-        for (auto& col : colConditions) {
-            adjcol[col[0]].insert(col[1]);
+        vector<set<int> > adjcol(k+1);    
+
+        for(auto col:colConditions){
+            int u=col[0],v=col[1];
+            adjcol[u].insert(v);
         }
 
         fill(vis.begin(), vis.end(), 0);
-        for (int i = 1; i <= k; i++) {
-            if (vis[i] == 0 && hasCycle(i, adjcol, vis)) {
+        for(int i=1;i<=k;i++){
+            if(vis[i]==0 && DFS(i,adjcol,vis)){
                 return {};
             }
         }
 
-        vector<int> rowOrder = topologicalsort(adjrow, k);
-        vector<int> colOrder = topologicalsort(adjcol, k);
+        vector<int> rowSort = topologicalsort(adjrow,k);
+        vector<int> colSort = topologicalsort(adjcol,k);
+        
+        unordered_map<int,int> rowIdx,colIdx;
 
-        unordered_map<int, int> rowPos, colPos;
-        for (int i = 0; i < rowOrder.size(); i++) rowPos[rowOrder[i]] = i;
-        for (int i = 0; i < colOrder.size(); i++) colPos[colOrder[i]] = i;
+        for(int i=0;i<k;i++) rowIdx[rowSort[i]]=i;
+        for(int i=0;i<k;i++) colIdx[colSort[i]]=i;
 
-        vector<vector<int>> matrix(k, vector<int>(k, 0));
-        for (int num = 1; num <= k; num++) {
-            matrix[rowPos[num]][colPos[num]] = num;
-        }
+        vector<vector<int> > mat(k,vector<int> (k,0));
 
-        return matrix;
+        for(int i=1;i<=k;i++){
+            int x=rowIdx[i];
+            int y=colIdx[i];
+            mat[x][y]=i;
+        } 
+
+        return mat;
+
+
     }
 };
