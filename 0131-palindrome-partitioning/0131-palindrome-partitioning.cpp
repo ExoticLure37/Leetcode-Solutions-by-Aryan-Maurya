bbccedb31fef7a338d747a2327da410acc445992
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2025-04-18 12:09:20 +0530

    Time: 156 ms (8.91%), Space: 167.3 MB (6.83%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -2,35 +2,37 @@ class Solution {
 public:
     int n;
     vector<vector<string> > res;
-    bool isPalin(string s,int l,int r)
-    {
-        while(l<r)
-        {
-            if(s[l++]!=s[r--])  return false;
+    bool isPalin(int i,int j,string &s){
+        if(j<i) return false;
+
+        if(i==j)    return true;
+
+        while(i<j){
+            if(s[i]!=s[j])  return false;
+            i++;
+            j--;
         }
         return true;
     }
-    void solve(int idx,string s,vector<string> & temp)
-    {
-        if(idx==n) 
-        {
-            res.push_back(temp);
+    void solve(int idx,string &s,vector<string > tmp){
+        if(idx==n){
+            res.push_back(tmp);
             return;
         }
-        for(int i=idx;i<n;i++)
-        {
-            if(isPalin(s,idx,i))
-            {
-                temp.push_back(s.substr(idx,i-idx+1));
-                solve(i+1,s,temp);
-                temp.pop_back();
+
+        for(int i=idx;i<n;i++){
+            if(isPalin(idx,i,s) && (i-idx+1)>0){
+                tmp.push_back(s.substr(idx,i-idx+1));
+                solve(i+1,s,tmp);
+                tmp.pop_back();
             }
         }
+        return;
     }
     vector<vector<string>> partition(string s) {
         n=s.length();
-        vector<string>temp;
-        solve(0,s,temp);
-        return res;
+        vector<string> tmp;
+        solve(0,s,tmp);
+        return res;        
     }
 };
