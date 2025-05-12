e86d55873e128b3523650abb4abc79813fabdb85
Aryan Maurya <127116742+ExoticLure37@users.noreply.github.com>
2025-05-12 09:22:02 +0530

    Time: 7 ms (58.45%), Space: 15.1 MB (30.63%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    const int N=10;
+    int freq[10]={0};
+    
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        for(auto c:digits)  freq[c]++;
+
+        vector<int> res;
+        for(int i=100;i<=999;i+=2){
+            int num=i;
+            bool found=true;
+            vector<int> tmp(N,0);
+            while(num){
+                int d=num%10;
+                tmp[d]++;
+                num/=10;
+            }
+
+            for(int i=0;i<10;i++){
+                if(tmp[i]>freq[i]) found=false;
+            }
+
+            if(found)   res.push_back(i);
+        }
+        
+        return res;
+    }
+};
